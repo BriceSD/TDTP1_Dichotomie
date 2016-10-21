30e426f48214cbc59542e18cffd3a7efdf713fef
SAUNIER DEBES Brice <brice.saunier@gmail.com>
2016-10-21 08:35:08 +0200

    Reformattage

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,31 +1,31 @@
-import com.sun.jmx.snmp.SnmpOidDatabaseSupport
+
 
 object Main {
 
-  def duplique (src: Array[Boolean], dst: Array[Boolean]): Unit ={
-    for (i<-0 to src.length)
+  def duplique(src: Array[Boolean], dst: Array[Boolean]): Unit = {
+    for (i <- 0 to src.length)
       dst(i) = src(i)
   }
 
-  def valeurTotale(objets: Array[Boolean], poids: Array[Int]): Int ={
+  def valeurTotale(objets: Array[Boolean], poids: Array[Int]): Int = {
     var total = 0
-    for (i<-objets.indices){
-      if(objets(i))
+    for (i <- objets.indices) {
+      if (objets(i))
         total += poids(i)
     }
     return total
   }
 
-  def affiche(objets: Array[Boolean], indiceMax: Int): Unit ={
-    for (i<-0 to math.min(indiceMax, objets.length-1)){
+  def affiche(objets: Array[Boolean], indiceMax: Int): Unit = {
+    for (i <- 0 to math.min(indiceMax, objets.length - 1)) {
       if (objets(i))
         print("O")
       else
         print("N")
     }
 
-    if (indiceMax < objets.length-1) {
-      for(i<- indiceMax until objets.length)
+    if (indiceMax < objets.length - 1) {
+      for (i <- indiceMax until objets.length)
         print("...")
     }
   }
