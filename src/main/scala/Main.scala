40fefcfd08ab29c93a73c8ecf638bc3af5544cfc
Nicolas Brandstett <nicolas.brandstett@telecomnancy.eu>
2016-10-21 08:37:05 +0200

    ezez

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,5 +5,21 @@ object Main {
     for (i<-0 to src.length)
       dst(i) = src(i)
   }
+  
+  def cherche(poid: Array[Int],capacite:Int)
+  {
+    var len = poids.length
+    var meilleure:Array[Boolean] = Array.fill(len)(false)
+    chercheRec(0,Array.fill(len)(false))
+    println
+    print("Meilleure solution trouvée")
+    affiche(meilleure,len)
+    print("Valeur du sac:"+valeurTotale(meilleur,poids))
+    
+    def chercheRec(profondeur:Int,sac_courant:Array[Boolean])
+    {
+      
+    }
+  }
 
 }
