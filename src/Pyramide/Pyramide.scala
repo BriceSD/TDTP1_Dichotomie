608e1f666bf6a223aa03bc82778c192e2fa9aff8
Nicolas Brandstett <nicolas.brandstett@telecomnancy.eu>
2016-11-09 16:33:37 +0100

    duplique

--- src/Pyramide/Pyramide.scala
+++ src/Pyramide/Pyramide.scala
@@ -1,3 +1,15 @@
 object Main {
-
+	
+  
+ def duplique(src: Array[Int]):Array[Int]={
+  var dist = Array.fill(src.length)(0)
+  for(i<-0 to src.length-1)
+  {
+    dist(i) = src(i)
+  }
+  return dist
 }
+  
+  
+}
+
