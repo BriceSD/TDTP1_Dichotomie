06cafbc414a091268f94abc2c93fcf054a875b96
Nicolas Brandstett <nicolas.brandstett@telecomnancy.eu>
2016-11-09 18:00:14 +0100

    FINI

--- src/Pyramide/Pyramide.scala
+++ src/Pyramide/Pyramide.scala
@@ -31,44 +31,39 @@ object Main {
             dejaPris = true
         }
         println("--------------")
-        print(dejaPris)
         if (!dejaPris) {
           tab(rang) = valeur
-          if(rang>=2)
-          {
+          
             
-            println(tab(rang-1))
-            println(tab(rang))
-            println(tab(rang-2))
-            println(math.abs((tab(rang-1)-tab(rang))))
-            if(math.abs((tab(rang-1)-tab(rang)))==tab(rang-2))
+            //println(tab(rang-1))
+            //println(tab(rang))
+            //println(tab(rang-2))
+            //println(math.abs((tab(rang-1)-tab(rang))))
+            if(correcte(tab,rang))
             { 
-              println("ici")
             	genere(rang + 1, tab)
             }
-          }
-          else
-          {
-            genere(rang + 1, tab)
-          }
+          
+          
          }
       }
     }
   }
 
-  def correcte(tab: Array[Int]): Boolean = {
+  def correcte(tab: Array[Int], rang:Int): Boolean = {
     var permutation = new String()
     for (i <- 0 to tab.length - 1)
       permutation += tab(i)
     for (ligne <- 1 to hauteur - 1) {
       for (col <- 1 to ligne) {
-        val n1 = tab(indice(ligne + 1, col))
-        val n2 = tab(indice(ligne + 1, col + 1))
-        val n3 = tab(indice(ligne, col))
+        if(indice(ligne+1,col+1)<=rang){
+        	val n1 = tab(indice(ligne + 1, col))
+        	val n2 = tab(indice(ligne + 1, col + 1))
+        	val n3 = tab(indice(ligne, col))
 
-        if (math.abs(n1 - n2) != n3)
-          return false
-      }
+        	if (math.abs(n1 - n2) != n3)
+        		return false
+      }}
     }
     return true
   }
